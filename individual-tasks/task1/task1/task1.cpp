71fce1ec1eb5b5687c25beb9054952d2a7f2ce18
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-02 11:25:47 +0300

    Ind task1

--- individual-tasks/task1/task1/task1.cpp
+++ individual-tasks/task1/task1/task1.cpp
@@ -8,162 +8,265 @@
 
 using namespace std;
 
-vector<double> solve(vector<vector<double>> m, vector<double> x, vector<double> b) {
-	vector<double> ans(x.size(), 0);
-	for (int i = 0; i < m.size(); i++) {
-		ans[i] = b[i];
-		for (int j = 0; j < m[i].size(); j++) {
-			if (i != j) {
-				ans[i] -= m[i][j] * x[j];
-			}
-		}
-		ans[i] /= m[i][i];
-	}
-	return ans;  
+int chank_size = 4;
+
+
+vector<double> solve_static(vector<vector<double>> m, vector<double> x, vector<double> b) {
+    vector<double> ans(x.size(), 0);
+
+#pragma omp parallel
+    {
+#pragma omp for schedule(static, chank_size)
+        for (int i = 0; i < m.size(); i++) {
+            ans[i] = b[i];
+            for (int j = 0; j < m[i].size(); j++) {
+                if (i != j) {
+                    ans[i] -= m[i][j] * x[j];
+                }
+            }
+            ans[i] /= m[i][i];
+        }
+    }
+    return ans;  
 }
 
-vector<double> diff_vec(vector<double> a, vector<double> b) {
-	vector<double> c(a.size(), 0);
-	for (int i = 0; i < a.size(); i++) {
-		c[i] = a[i] - b[i];
+vector<double> diff_vec_static(vector<double> a, vector<double> b) {
+    vector<double> c(a.size(), 0);
+#pragma omp parallel
+	{
+#pragma omp for schedule(static, chank_size)
+		for (int i = 0; i < a.size(); i++) {
+			c[i] = a[i] - b[i];
+		}
 	}
-	return c;
+    return c;
 }
 
 double norma(vector<double> a) {
-	double max = a[0];
+    double max = a[0];
 	for (int i = 1; i < a.size(); i++) {
 		if (a[i] > max) {
 			max = a[i];
 		}
 	}
-	return max;
+    return max;
 }
-/*
-int main()
-{
-	cout << "Enter file name: " << endl;
-	string fileName;
-	cin >> fileName;
-	ifstream file;
-	file.open(fileName + ".txt");
 
-	int n;
-	file >> n;
 
-	vector<vector<double>> matrix;
-	vector<double> b(n, 0);
-	matrix.resize(n);
-	for (int i = 0; i < n; i++) {
-		matrix[i].resize(n);
-		for (int j = 0; j < n; j++) {
-			file >> matrix[i][j];
-		}
-		file >> b[i];
-	}
-	
-	vector<double> x1(n, 0), x2 = solve(matrix, x1, b), x0;
+vector<double> solve_dynamic(vector<vector<double>> m, vector<double> x, vector<double> b) {
+    vector<double> ans(x.size(), 0);
 
-	while (abs(norma(diff_vec(x2, x1))) > 0.00001) {
-		x1 = x2;
-		x2 = solve(matrix, x2, b);
-		cout << "=========================================" << endl;
-		for (int i = 0; i < n; i++) {
-			cout << x2[i] << " ";
-		}
-		cout << endl;
-	}
-
-	cout << "=========================================" << endl;
-	for (int i = 0; i < n; i++) {
-		cout << x2[i] << " ";
-	}
-}
-*/
-int main()
-{
-	cout << "Enter file name: " << endl;
-	string fileName;
-	cin >> fileName;
-	ifstream file;
-	file.open(fileName + ".txt");
-
-    int size;
-    file >> size;
-
-    vector <vector <long double> > matrix;
-
-    matrix.resize(size);
-    for (int i = 0; i < size; i++)
+#pragma omp parallel
     {
-        matrix[i].resize(size + 1);
-
-        for (int j = 0; j < size + 1; j++)
-        {
-            file >> matrix[i][j];
-        }
-    }
-	cout << size << endl;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size + 1; j++) {
-			cout << matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
-
-    long double eps;
-    cin >> eps;
-
-    vector <long double> previousVariableValues(size, 0.0);
-
-    while (true)
-    {
-        vector <long double> currentVariableValues(size);
-
-
-        for (int i = 0; i < size; i++)
-        {
-            currentVariableValues[i] = matrix[i][size];
-
-            for (int j = 0; j < size; j++)
-            {
-                if (i != j)
-                {
-                    currentVariableValues[i] -= matrix[i][j] * previousVariableValues[j];
+#pragma omp for schedule(dynamic, chank_size)
+        for (int i = 0; i < m.size(); i++) {
+            ans[i] = b[i];
+            for (int j = 0; j < m[i].size(); j++) {
+                if (i != j) {
+                    ans[i] -= m[i][j] * x[j];
                 }
             }
-
-            currentVariableValues[i] /= matrix[i][i];
+            ans[i] /= m[i][i];
         }
+    }
+    return ans;  
+}
 
-        long double error = 0.0;
-
-        for (int i = 0; i < size; i++)
-        {
-            error += abs(currentVariableValues[i] - previousVariableValues[i]);
-        }
-
-        if (error < eps)
-        {
-            break;
-        }
-
-        previousVariableValues = currentVariableValues;
-
-		for (int i = 0; i < size; i++) {
-			cout << currentVariableValues[i] << " ";
+vector<double> diff_vec_dynamic(vector<double> a, vector<double> b) {
+    vector<double> c(a.size(), 0);
+#pragma omp parallel
+	{
+#pragma omp for schedule(dynamic, chank_size)
+		for (int i = 0; i < a.size(); i++) {
+			c[i] = a[i] - b[i];
 		}
-		cout << endl;
-    }
+	}
+    return c;
+}
 
-    for (int i = 0; i < size; i++)
+vector<double> solve_guided(vector<vector<double>> m, vector<double> x, vector<double> b) {
+    vector<double> ans(x.size(), 0);
+
+#pragma omp parallel
     {
-        printf("%.8llf ", previousVariableValues[i]);
+#pragma omp for schedule(guided, chank_size)
+        for (int i = 0; i < m.size(); i++) {
+            ans[i] = b[i];
+            for (int j = 0; j < m[i].size(); j++) {
+                if (i != j) {
+                    ans[i] -= m[i][j] * x[j];
+                }
+            }
+            ans[i] /= m[i][i];
+        }
     }
-	
-	file.close();
+    return ans;  
+}
 
-    return 0;
+vector<double> diff_vec_guided(vector<double> a, vector<double> b) {
+    vector<double> c(a.size(), 0);
+#pragma omp parallel
+	{
+#pragma omp for schedule(guided, chank_size)
+		for (int i = 0; i < a.size(); i++) {
+			c[i] = a[i] - b[i];
+		}
+	}
+    return c;
+}
+
+int main()
+{
+    /*
+    cout << "Enter file name: " << endl;
+    string fileName;
+    cin >> fileName;
+    ifstream file;
+    file.open(fileName + ".txt");
+
+    int n;
+    file >> n;
+    */
+	
+	ofstream file;
+	file.open("static.txt");
+
+	file << "static" << endl;
+	file << "threads, \"chank size\", time " << endl;
+
+	for (int i = 2; i <= 4; i++) {
+		omp_set_num_threads(i);
+		for (chank_size = 100; chank_size >= 10; chank_size -= 10) {
+			int n = 3000;
+			vector<vector<double>> matrix;
+			vector<double> b(n, 0);
+			matrix.resize(n);
+			for (int i = 0; i < n; i++) {
+				matrix[i].resize(n);
+				for (int j = 0; j < n; j++) {
+					//file >> matrix[i][j];
+					if (i == j) {
+						matrix[i][j] = (double)i + j + 1;
+						matrix[i][j] *= n;
+					}
+					else {
+						matrix[i][j] = 1;
+					}
+				}
+				//file >> b[i];
+				b[i] = i;
+			}
+
+			double time_start = omp_get_wtime();
+
+			vector<double> x1(n, 0), x2 = solve_static(matrix, x1, b), x0;
+
+			while (abs(norma(diff_vec_static(x2, x1))) > 0.00001) {
+				x1 = x2;
+				x2 = solve_static(matrix, x2, b);
+			}
+
+			double time_end = omp_get_wtime();
+
+			//for (int i = 0; i < n; i++) {
+			//	cout << x2[i] << " ";
+			//}
+
+			file << i << ", " << chank_size << ", " << time_end - time_start << endl;
+		}
+	}
+
+
+	file << "dyanmic" << endl;
+	file << "threads, \"chank size\", time " << endl;
+
+	for (int i = 2; i <= 4; i++) {
+		omp_set_num_threads(i);
+		for (chank_size = 100; chank_size >= 10; chank_size -= 10) {
+			int n = 3000;
+			vector<vector<double>> matrix;
+			vector<double> b(n, 0);
+			matrix.resize(n);
+			for (int i = 0; i < n; i++) {
+				matrix[i].resize(n);
+				for (int j = 0; j < n; j++) {
+					//file >> matrix[i][j];
+					if (i == j) {
+						matrix[i][j] = (double)i + j + 1;
+						matrix[i][j] *= n;
+					}
+					else {
+						matrix[i][j] = 1;
+					}
+				}
+				//file >> b[i];
+				b[i] = i;
+			}
+
+			double time_start = omp_get_wtime();
+
+			vector<double> x1(n, 0), x2 = solve_dynamic(matrix, x1, b), x0;
+
+			while (abs(norma(diff_vec_dynamic(x2, x1))) > 0.00001) {
+				x1 = x2;
+				x2 = solve_dynamic(matrix, x2, b);
+			}
+
+			double time_end = omp_get_wtime();
+
+			//for (int i = 0; i < n; i++) {
+			//	cout << x2[i] << " ";
+			//}
+
+			file << i << ", " << chank_size << ", " << time_end - time_start << endl;
+		}
+	}
+
+	file << "guided" << endl;
+	file << "threads, \"chank size\", time " << endl;
+
+	for (int i = 2; i <= 4; i++) {
+		omp_set_num_threads(i);
+		for (chank_size = 100; chank_size >= 10; chank_size -= 10) {
+			int n = 3000;
+			vector<vector<double>> matrix;
+			vector<double> b(n, 0);
+			matrix.resize(n);
+			for (int i = 0; i < n; i++) {
+				matrix[i].resize(n);
+				for (int j = 0; j < n; j++) {
+					//file >> matrix[i][j];
+					if (i == j) {
+						matrix[i][j] = (double)i + j + 1;
+						matrix[i][j] *= n;
+					}
+					else {
+						matrix[i][j] = 1;
+					}
+				}
+				//file >> b[i];
+				b[i] = i;
+			}
+
+			double time_start = omp_get_wtime();
+
+			vector<double> x1(n, 0), x2 = solve_guided(matrix, x1, b), x0;
+
+			while (abs(norma(diff_vec_guided(x2, x1))) > 0.00001) {
+				x1 = x2;
+				x2 = solve_guided(matrix, x2, b);
+			}
+
+			double time_end = omp_get_wtime();
+
+			//for (int i = 0; i < n; i++) {
+			//	cout << x2[i] << " ";
+			//}
+
+			file << i << ", " << chank_size << ", " << time_end - time_start << endl;
+		}
+	}
+	file.close();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
